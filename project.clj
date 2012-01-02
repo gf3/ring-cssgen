b7bd370e4867a92ce8475062c12050a536c6a687
Gianni Chiappetta <gianni@runlevel6.org>
2012-01-02 05:46:57 -0500

    add: Extra info to project.clj.

--- project.clj
+++ project.clj
@@ -1,9 +1,21 @@
 (defproject ring-cssgen "0.0.1-SNAPSHOT"
+
   :description "Ring middleware to automatically regenerate cssgen stylesheets."
+
+  :url "https://github.com/gf3/ring-cssgen"
+
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/tools.namespace "0.1.0"]
                  [cssgen "0.3.0-SNAPSHOT"]]
+
   :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]
                      [speclj "1.5.2"]]
+
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+
   :test-path "spec/"
+
   :main ring-cssgen.core)
